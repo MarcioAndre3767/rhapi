545ba9b4acef31b44e369d769ebe669b7a69cf2b
Marcio Andre Mendes <marcioandre.ti1@gmail.com>
2023-09-09 19:50:11 -0300

    criação do CRUD, entidade, controler e repositorie

---
+++ src/main/java/com/devmam/rh/ApiRhApplication.java
@@ -0,0 +1,16 @@
+package com.devmam.rh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiRhApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiRhApplication.class, args);
+	}
+
+}
+
+
+
---
+++ src/main/java/com/devmam/rh/model/StatusFuncionario.java
@@ -0,0 +1,5 @@
+package com.devmam.rh.model;
+
+public enum StatusFuncionario {
+    ATIVO, INATIVO
+}
